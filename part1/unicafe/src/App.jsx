2ec202611e75b0687128c76ea99c8c7736196dfa
DaPurr <git@milovanovic.nl>
2024-07-03 10:44:36 +0200

    Add exercise 1.8

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -6,15 +6,7 @@ const Statistic = ({ text, value }) => {
   )
 }
 
-const App = () => {
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-
-  const incrementGood = () => setGood(good + 1)
-  const incrementNeutral = () => setNeutral(neutral + 1)
-  const incrementBad = () => setBad(bad + 1)
-
+const Statistics = ({ good, neutral, bad }) => {
   const totalFeedback = good + neutral + bad
 
   const calculateAverage = () => {
@@ -29,11 +21,6 @@ const App = () => {
 
   return (
     <div>
-      <h1>give feedback</h1>
-      <button onClick={incrementGood}>good</button>
-      <button onClick={incrementNeutral}>neutral</button>
-      <button onClick={incrementBad}>bad</button>
-
       <h1>statistics</h1>
       <Statistic text="good" value={good} />
       <Statistic text="neutral" value={neutral} />
@@ -41,6 +28,28 @@ const App = () => {
       <Statistic text="all" value={totalFeedback} />
       <Statistic text="average" value={calculateAverage()} />
       <Statistic text="positive" value={calculatePercentagePositive()} />
+    </div>
+  )
+}
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  const incrementGood = () => setGood(good + 1)
+  const incrementNeutral = () => setNeutral(neutral + 1)
+  const incrementBad = () => setBad(bad + 1)
+
+  const hasFeedback = good + neutral + bad > 0
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <button onClick={incrementGood}>good</button>
+      <button onClick={incrementNeutral}>neutral</button>
+      <button onClick={incrementBad}>bad</button>
+      {hasFeedback && <Statistics good={good} neutral={neutral} bad={bad} />}
     </div>
   )
 }
