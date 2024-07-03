23ffd6692855d035b73ec645bf4edb132e0de566
DaPurr <git@milovanovic.nl>
2024-07-03 13:31:16 +0200

    Add exercise 2.1

---
+++ part2/courseinfo/src/App.jsx
@@ -0,0 +1,68 @@
+const Header = ({ name }) => {
+  return (
+    <h1>{name}</h1>
+  )
+}
+
+const Part = ({ part }) => {
+  return (
+    <p>
+      {part.name} {part.exercises}
+    </p>
+  )
+}
+
+const Content = ({ parts }) => {
+  return (
+    parts.map((part, index) =>
+      <Part key={index} part={part} />)
+  )
+}
+
+const Total = ({ exercises }) => {
+  return (
+    <p>Number of exercises {exercises.reduce((accum, x) => accum + x, 0)}</p>
+  )
+}
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header name={course.name} />
+      <Content parts={course.parts} />
+      <Total exercises={course.parts.map(part => part.exercises)} />
+    </div>
+  )
+}
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        id: 1,
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        id: 2,
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        id: 3,
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Course course={course} />
+    </div>
+  )
+}
+
+export default App
