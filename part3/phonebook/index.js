f00c01fb68f3becc5315bbf8464e01b54ed5a433
DaPurr <git@milovanovic.nl>
2024-07-04 14:58:16 +0200

    Add cors and prepare for deployment

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
 morgan.token('body', request => JSON.stringify(request.body))
 
+app.use(cors())
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
@@ -85,7 +87,7 @@ const generateId = () => {
     return Math.floor(Math.random() * 10000)
 }
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 })
