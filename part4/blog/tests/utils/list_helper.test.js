d9ce5babfc781b56692e8dc632f3d5ceb80961dc
DaPurr <git@milovanovic.nl>
2024-07-05 11:39:35 +0200

    Add exercise 4.4

--- part4/blog/tests/utils/list_helper.test.js
+++ part4/blog/tests/utils/list_helper.test.js
@@ -1,4 +1,4 @@
-const { test } = require('node:test')
+const { test, describe } = require('node:test')
 const assert = require('node:assert')
 const listHelper = require('../../utils/list_helper')
 
@@ -7,4 +7,68 @@ test('dummy returns one', () => {
 
   const result = listHelper.dummy(blogs)
   assert.strictEqual(result, 1)
+})
+
+describe('total likes', () => {
+  const listWithOneBlog = [
+    {
+      id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+      likes: 5,
+    },
+  ]
+
+  const listWithTwoBlogs = [
+    {
+      id: '5a422a851b54a676234d17f7',
+      title: 'React patterns',
+      author: 'Michael Chan',
+      url: 'https://reactpatterns.com/',
+      likes: 7,
+    },
+    {
+      id: '5a422b3a1b54a676234d17f9',
+      title: 'Canonical string reduction',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+      likes: -12,
+    },
+  ]
+
+  const listWithoutLikes = [
+    {
+      id: '5a422a851b54a676234d17f7',
+      title: 'React patterns',
+      author: 'Michael Chan',
+      url: 'https://reactpatterns.com/',
+    },
+    {
+      id: '5a422b3a1b54a676234d17f9',
+      title: 'Canonical string reduction',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+      likes: 10,
+    },
+  ]
+
+  test('when list is empty, equals 0', () => {
+    assert.strictEqual(listHelper.totalLikes([]), 0)
+  })
+
+  test('when list has only one blog, equals the likes of that', () => {
+    const result = listHelper.totalLikes(listWithOneBlog)
+    assert.strictEqual(result, 5)
+  })
+
+  test('when list has two blogs with negative numbers, equals sum of all likes', () => {
+    const result = listHelper.totalLikes(listWithTwoBlogs)
+    assert.strictEqual(result, -5)
+  })
+
+  test('when list has blogs missing likes, equals NaN', () => {
+    const result = listHelper.totalLikes(listWithoutLikes)
+    assert.strictEqual(result, NaN)
+  })
 })
--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -2,6 +2,10 @@ const dummy = () => {
   return 1
 }
 
+const totalLikes = blogs =>
+  blogs.map(blog => blog.likes).reduce((accum, x) => accum + x, 0)
+
 module.exports = {
   dummy,
+  totalLikes,
 }
