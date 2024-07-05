f5bb63a391af93ea6c74edcf67ddbacdb310c1f3
DaPurr <git@milovanovic.nl>
2024-07-05 12:01:23 +0200

    Add exercise 4.5

--- part4/blog/tests/utils/list_helper.test.js
+++ part4/blog/tests/utils/list_helper.test.js
@@ -37,7 +37,7 @@ describe('total likes', () => {
     },
   ]
 
-  const listWithoutLikes = [
+  const listMissingLikes = [
     {
       id: '5a422a851b54a676234d17f7',
       title: 'React patterns',
@@ -68,7 +68,90 @@ describe('total likes', () => {
   })
 
   test('when list has blogs missing likes, equals NaN', () => {
-    const result = listHelper.totalLikes(listWithoutLikes)
+    const result = listHelper.totalLikes(listMissingLikes)
     assert.strictEqual(result, NaN)
+  })
+})
+
+describe('favorite blog', () => {
+  const listWithOneBlog = [
+    {
+      id: '5a422aa71b54a676234d17f8',
+      title: 'Go To Statement Considered Harmful',
+      author: 'Edsger W. Dijkstra',
+      url: 'https://homepages.cwi.nl/~storm/teaching/reader/Dijkstra68.pdf',
+      likes: 5,
+    },
+  ]
+
+  const listWithTwoBlogs = [
+    {
+      id: '5a422a851b54a676234d17f7',
+      title: 'React patterns',
+      author: 'Michael Chan',
+      url: 'https://reactpatterns.com/',
+      likes: 7,
+    },
+    {
+      id: '5a422b3a1b54a676234d17f9',
+      title: 'Canonical string reduction',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+      likes: -12,
+    },
+  ]
+
+  const listMissingLikes = [
+    {
+      id: '5a422a851b54a676234d17f7',
+      title: 'React patterns',
+      author: 'Michael Chan',
+      url: 'https://reactpatterns.com/',
+    },
+    {
+      id: '5a422b3a1b54a676234d17f9',
+      title: 'Canonical string reduction',
+      author: 'Edsger W. Dijkstra',
+      url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+      likes: 10,
+    },
+  ]
+
+  const listWithoutLikes = [
+    {
+      id: '5a422a851b54a676234d17f7',
+      title: 'React patterns',
+      author: 'Michael Chan',
+      url: 'https://reactpatterns.com/',
+    },
+  ]
+
+  test('when there is no blog, return undefined', () => {
+    assert.deepStrictEqual(listHelper.favoriteBlog([]), undefined)
+  })
+
+  test('when there is only one blog, return that one', () => {
+    assert.deepStrictEqual(
+      listHelper.favoriteBlog(listWithOneBlog),
+      listWithOneBlog[0]
+    )
+  })
+
+  test('when there are two blogs, return the most popular one', () => {
+    assert.deepStrictEqual(
+      listHelper.favoriteBlog(listWithTwoBlogs),
+      listWithTwoBlogs[0]
+    )
+  })
+
+  test('when there is a blog missing likes, return the most popular one', () => {
+    assert.deepStrictEqual(
+      listHelper.favoriteBlog(listMissingLikes),
+      listMissingLikes[1]
+    )
+  })
+
+  test('when there is a list missing likes, return undefined', () => {
+    assert.deepStrictEqual(listHelper.favoriteBlog(listWithoutLikes), undefined)
   })
 })
--- part4/blog/utils/list_helper.js
+++ part4/blog/utils/list_helper.js
@@ -5,7 +5,19 @@ const dummy = () => {
 const totalLikes = blogs =>
   blogs.map(blog => blog.likes).reduce((accum, x) => accum + x, 0)
 
+const favoriteBlog = blogs => {
+  if (blogs.length === 0) return undefined
+  const initialBlog = blogs.find(blog => blog.likes !== undefined)
+  if (initialBlog === undefined) return undefined
+  return blogs.reduce(
+    (mostPopularBlog, currentBlog) =>
+      currentBlog.likes > mostPopularBlog.likes ? currentBlog : mostPopularBlog,
+    initialBlog
+  )
+}
+
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
 }
