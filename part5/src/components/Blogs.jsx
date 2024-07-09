7b7c84fe06d2633bbb1e7c19bf4104cec5050e8f
DaPurr <git@milovanovic.nl>
2024-07-09 23:14:49 +0200

    Add exercise 5.10

--- part5/src/components/Blogs.jsx
+++ part5/src/components/Blogs.jsx
@@ -1,9 +1,14 @@
 import Blog from './Blog'
 
 const Blogs = ({ blogs, deleteWithId, likeWithId }) => {
+  const sortedBlogs = [...blogs].sort((blogA, blogB) => {
+    const likesA = blogA.likes ? blogA.likes : 0
+    const likesB = blogB.likes ? blogB.likes : 0
+    return likesB - likesA
+  })
   return (
     <div>
-      {blogs.map(blog => (
+      {sortedBlogs.map(blog => (
         <Blog
           key={blog.id}
           blog={blog}
