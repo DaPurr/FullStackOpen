6fd5c4a7ae75ed93ec495593ede57e5448d761b5
DaPurr <git@milovanovic.nl>
2024-07-12 15:49:45 +0200

    Add exercises 6.16 and 6.17

--- part6/anecdotes-redux/src/components/AnecdoteForm.jsx
+++ part6/anecdotes-redux/src/components/AnecdoteForm.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
-import { addNew } from '../reducers/anecdoteReducer'
+import { createAnecdote } from '../reducers/anecdoteReducer'
 import { useDispatch } from 'react-redux'
-import anecdoteService from '../services/anecdotes'
 
 const AnecdoteForm = () => {
   const dispatch = useDispatch()
@@ -12,8 +11,7 @@ const AnecdoteForm = () => {
 
     event.preventDefault()
 
-    const response = await anecdoteService.createNew(anecdoteContent)
-    dispatch(addNew(response.data))
+    dispatch(createAnecdote(anecdoteContent))
   }
 
   return (
--- part6/anecdotes-redux/src/reducers/anecdoteReducer.js
+++ part6/anecdotes-redux/src/reducers/anecdoteReducer.js
@@ -1,13 +1,13 @@
 import { createSlice } from '@reduxjs/toolkit'
 import anecdoteService from '../services/anecdotes'
 
-// helpers
-const initialState = await anecdoteService.findAll()
-
 const anecdoteSlice = createSlice({
   name: 'anecdotes',
-  initialState,
+  initialState: [],
   reducers: {
+    setAnecdotes(state, action) {
+      return action.payload
+    },
     voteFor(state, action) {
       return state.map(anecdote => {
         if (anecdote.id === action.payload) {
@@ -29,5 +29,19 @@ const anecdoteSlice = createSlice({
   },
 })
 
+export const initializeAnecdotes = () => {
+  return async dispatch => {
+    const anecdotes = await anecdoteService.findAll()
+    dispatch(setAnecdotes(anecdotes))
+  }
+}
+
+export const createAnecdote = content => {
+  return async dispatch => {
+    const response = await anecdoteService.createNew(content)
+    dispatch(addNew(response.data))
+  }
+}
+
 export default anecdoteSlice.reducer
-export const { voteFor, addNew } = anecdoteSlice.actions
+export const { voteFor, addNew, setAnecdotes } = anecdoteSlice.actions
